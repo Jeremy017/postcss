a8548ac5e1ea833d0afaebe4f101e58c4a96a0d0
xwillmadeit <xiesen01@outlook.com>
2016-03-08 14:43:09 +0800

    watch

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,29 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssnext = require('cssnext');
 var precss = require('precss');
+var atImport = require('postcss-import');
+var cssnano = require('cssnano');
+var watch = require('gulp-watch');
+
+var path = {
+	sass: ['./src/*.css']
+}
 
 gulp.task('style',function(){
 	var processors = [
 		autoprefixer,
 		precss,
-		cssnext
+		cssnext,
+		atImport,
+		cssnano
 	];
-	return gulp.src('./src/*.css')
+	return gulp.src(path.sass)
 	.pipe(postcss(processors))
 	.pipe(gulp.dest('./dest'));
 });
+
+gulp.task('watch', function () {
+    gulp.watch(path.sass,['style']);
+});
+
+gulp.task('default',['watch']);
